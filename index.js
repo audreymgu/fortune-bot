2f0dbf1fa955cd8ef0be5fab52c89cc05fd52ba8
Audrey Gu <audreymgu@gmail.com>
2026-06-02 19:04:21 -0400

    Restore default behavior.

--- index.js
+++ index.js
@@ -46,12 +46,11 @@ app.post(["/tell"], (req, res) => {
   if (lm.commandBuffer.length == 0) {
     const message = messages[Math.floor(Math.random() * messages.length)];
     let symbol;
-    // if (message.symbol) {
-    //   symbol = symbols.find((data) => data.name == message.symbol);
-    // } else {
-    //   symbol = symbols[Math.floor(Math.random() * symbols.length)];
-    // }
-    symbol = symbols.find((data) => data.name == "crab");
+    if (message.symbol) {
+      symbol = symbols.find((data) => data.name == message.symbol);
+    } else {
+      symbol = symbols[Math.floor(Math.random() * symbols.length)];
+    }
 
     res.status(200).json({
       message: message.message,
